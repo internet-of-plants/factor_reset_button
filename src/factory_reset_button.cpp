57b448fe1526ae018f77461ed1c82a29ccae9a42
Paulo Cabral Sanz <paulosanz@poli.ufrj.br>
2022-04-09 23:39:40 -0300

    Fix typo

--- src/factory_reset_button.cpp
+++ src/factory_reset_button.cpp
@@ -26,7 +26,7 @@ void IOP_RAM buttonChanged() noexcept {
       iop::Log::print("[INFO] RESET: Press this button for 15 seconds more to factory reset the device\n", iop::LogLevel::INFO, iop::LogType::STARTEND);
     }
   } else if (resetStateTime < iop_hal::thisThread.timeRunning()) {
-    *factoryResetFlag = true;
+    factoryResetFlag = true;
 
     if (IOP_LOG_LEVEL <= iop::LogLevel::INFO) {
       iop::Log::print("[INFO] RESET: Factory reset scheduled, it will run in the next loop run\n", iop::LogLevel::INFO, iop::LogType::STARTEND);
